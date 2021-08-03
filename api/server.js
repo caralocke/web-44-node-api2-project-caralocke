e2369e00e3787376dcad3ee250a591e0d9e1bb01
Cara M Locke <81659057+caralocke@users.noreply.github.com>
2021-08-03 10:54:54 -0400

    Added required code to both server.js and posts-router.js to allow endpoints with /api/posts to be routed through posts-router.

--- api/server.js
+++ api/server.js
@@ -4,5 +4,5 @@ const express =  require('express')
 const postsRouter = require('./posts/posts-router')
 const server = express()
 server.use(express.json())
-
+server.use('/api/posts', postsRouter)
 module.exports = server
