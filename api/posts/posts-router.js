af6047ac3126e65858648480dcad34a5c46da256
Cara M Locke <81659057+caralocke@users.noreply.github.com>
2021-08-03 12:42:55 -0400

    Completed setting up endpoint for editing a post.

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -47,4 +47,26 @@ router.post('/', (req, res) => {
     }
 })
 
+router.put('/:id', (req, res) => {
+    if(!req.body.title || !req.body.contents) {
+        res.status(400).json({ message: 'Please provide title and contents for the post'})
+    } else {
+    const changes = req.body
+    Posts.update(req.params.id, changes)
+        .then(updated => {
+            if(updated) {
+                Posts.findById(updated)
+                    .then(post => {
+                        res.status(200).json(post)
+                    })
+            } else {
+                res.status(404).json({ message: 'The post with the specified ID does not exist'})
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ message: 'The post information could not be modified'})
+        })
+    }
+})
+
 module.exports = router
