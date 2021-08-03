fc6beb2df0a26a6c67f996a2a3c340765898e439
Cara M Locke <81659057+caralocke@users.noreply.github.com>
2021-08-03 13:35:01 -0400

    Completed setting up the endpoint for getting comments.

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -69,4 +69,37 @@ router.put('/:id', (req, res) => {
     }
 })
 
+router.delete('/:id', (req, res) => {
+    const id = req.params.id
+    Posts.findById(id)
+        .then(post => {
+            if(post) {
+                Posts.remove(id)
+                    .then(deleted => {
+                        res.status(200).json(post)
+                    })
+            } else {
+                res.status(404).json({ message: 'The post with the specified ID does not exist'})
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ message: 'The post could not be removed'})
+        })
+})
+
+router.get('/:id/comments', (req, res) => {
+    const id = req.params.id
+    Posts.findPostComments(id)
+        .then(post => {
+            if(post.length) {
+                res.status(200).json(post)
+            } else {
+                res.status(404).json({ message: 'The post with the specified ID does not exist'})
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ message: 'The comments information could not be retrieved'})
+        })
+})
+
 module.exports = router
