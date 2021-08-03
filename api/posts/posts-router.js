5026c44475ca51f4a0c9666114612b47812d1551
Cara M Locke <81659057+caralocke@users.noreply.github.com>
2021-08-03 11:04:30 -0400

    Completed endpoint for getting all posts.

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -4,4 +4,13 @@ const Posts = require('./posts-model')
 
 const router = express.Router()
 
+router.get('/', (req, res) => {
+    Posts.find()
+        .then(posts => {
+            res.status(200).json(posts)
+        })
+        .catch(err => {
+            res.status(500).json({ message: 'The posts information could not be retrieved'})
+        })
+})
 module.exports = router
