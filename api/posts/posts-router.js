f3a2d0d052aca707bb78d4da574cb1a687693235
Cara M Locke <81659057+caralocke@users.noreply.github.com>
2021-08-03 11:22:58 -0400

    Completed endpoint for getting posts by id.

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -13,4 +13,19 @@ router.get('/', (req, res) => {
             res.status(500).json({ message: 'The posts information could not be retrieved'})
         })
 })
+
+router.get('/:id', (req, res) => {
+    const id = req.params.id
+    Posts.findById(id)
+        .then(post => {
+          if(post) {
+            res.status(200).json(post)
+          } else {
+              res.status(404).json({message: 'the post with the specified ID does not exist'})
+          }
+        })
+        .catch(err => {
+            res.status(500).json({ message: 'The post information could not be retrieved'})
+        })
+})
 module.exports = router
